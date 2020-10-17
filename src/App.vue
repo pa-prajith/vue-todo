b7b3ac9ec06b4c339e91a57acae1773c0e862519
pa-prajith <codejs100@gmail.com>
2020-10-16 23:10:25 -0700

    Added design for Todo app

--- src/App.vue
+++ src/App.vue
@@ -9,14 +9,53 @@
       <button>Add Todo</button>
     </form>
   </section>
+  <section class="todoList">
+    <ul>
+      <li>
+        <div class="todo-item">Todo Item List goes here</div>
+        <div class="icons">
+          <i class="far fa-check-circle"></i>
+          <i class="far fa-edit"></i>
+          <i class="fas fa-trash"></i>
+        </div>
+      </li>
+      <li>
+        <div class="todo-item done">Todo Item List goes here. Content goes here. This is a long text!!!. Now what , lets see how it looks on page </div>
+        <div class="icons">
+          <i class="far fa-check-circle"></i>
+          <i class="far fa-edit"></i>
+          <i class="fas fa-trash"></i>
+        </div>
+      </li>
+      <li>
+        <div class="todo-item">Todo Item List goes here</div>
+        <div class="icons">
+          <i class="far fa-check-circle"></i>
+          <i class="far fa-edit"></i>
+          <i class="fas fa-trash"></i>
+        </div>
+      </li>
+    </ul>
+  </section>
 </div>
 
 </template>
 
 <script>
 
+import { ref } from 'vue';
+
 export default {
   name: 'App',
+  setup() {
+    const newList = ref('');
+    const todoArray = ref([]);
+    
+    return {
+      newList,
+      todoArray
+    }
+  }
 }
 
 </script>
@@ -50,6 +89,10 @@ header {
   padding: 10px;
   border: 1px solid #2c3e50;
   border-radius: 15px;
+  font-weight: 500;
+  font-size: 21px;
+  outline: none;
+  
 }
 
 .form-container button {
@@ -60,4 +103,36 @@ header {
   font-weight: bold;
 }
 
+/* Todolist Container */
+.todoList {
+  width: 55%;
+  padding: 50px;
+  margin: 0 auto; 
+  font-size: 25px;
+}
+
+.todoList ul li {
+  list-style: none;
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+  border-bottom: 1px solid #cccccc;
+}
+
+.done {
+  text-decoration: line-through;
+}
+
+.todo-item {
+  font-weight: 500;
+}
+
+.icons {
+  white-space: nowrap;;
+}
+
+.icons i{
+  margin-left: 15px;
+  cursor: pointer;
+}
 </style>
